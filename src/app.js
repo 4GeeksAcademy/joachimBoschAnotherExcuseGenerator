eb0646437b08dafa7f63d500d5f5bc69eba9f838
BoschJ <bosch.joachim@outlook.com>
2024-05-17 20:41:51 +0200

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let part1 = math.floor(math.random() * who.length);
+  let part2 = math.floor(math.random() * action.length);
+  let part3 = math.floor(math.random() * what.length);
+  let part4 = math.floor(math.random() * when.length);
+
+  let excuse = document.write(`${who[part1]}${action[part2]}${what[part3]}${when[part4]}`);
+
+  let sentence = document.getElementById("excuse");
+  sentence.innerHTML = excuse;
+
+  onload()
 };
+
+
