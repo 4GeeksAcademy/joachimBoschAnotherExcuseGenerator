ab5f7cdc2f0e8b98cd8fb6bfc57b02cc75dcf635
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-05-17 18:57:04 +0000

    Linked app.js file to index.html

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 window.onload = function() {
   //write your code here
-  
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let part1 = math.floor(math.random() * who.length);
-  let part2 = math.floor(math.random() * action.length);
-  let part3 = math.floor(math.random() * what.length);
-  let part4 = math.floor(math.random() * when.length);
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class.",
+    "when I was sleeping.",
+    "while I was exercising.",
+    "during my lunch.",
+    "while I was praying."
+  ];
 
-  let excuse = document.write(`${who[part1]}${action[part2]}${what[part3]}${when[part4]}`);
+  let part1 = Math.floor(Math.random() * who.length);
+  let part2 = Math.floor(Math.random() * action.length);
+  let part3 = Math.floor(Math.random() * what.length);
+  let part4 = Math.floor(Math.random() * when.length);
+
+  let excuse = document.write(
+    `${who[part1]} ${action[part2]} ${what[part3]} ${when[part4]}`
+  );
 
   let sentence = document.getElementById("excuse");
   sentence.innerHTML = excuse;
-
-  onload()
 };
-
-
